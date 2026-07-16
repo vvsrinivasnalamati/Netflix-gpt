e97890463073a51df3247ca8520d67418a8db040
vvsrinivasnalamati <vvsrinivasnalamati@gmail.com>
2026-07-17 05:19:11 +0530

    added sample text to verify the app

--- App.js
+++ App.js
@@ -1,3 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 const AppLayout = () => {
-  return <div></div>;
+  return (
+    <div>
+      <h1> Wel come to Net flix</h1>
+    </div>
+  );
 };
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<AppLayout />);
